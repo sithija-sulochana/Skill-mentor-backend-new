60a32e504107532aaa46d4429e136e145e0da4af
sithija_sulochana <144917814+sithija-sulochana@users.noreply.github.com>
2026-01-29 22:15:29 +0530

    Add test configuration and dependencies for H2 database and JWT

--- src/test/java/com/example/springpractice/SpringpracticeApplicationTests.java
+++ src/test/java/com/example/springpractice/SpringpracticeApplicationTests.java
@@ -2,14 +2,12 @@ package com.example.springpractice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(
-        properties = {
-                "spring.autoconfigure.exclude=" +
-                        "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
-                        "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
-        }
-)
+@SpringBootTest()
+
+
+@ActiveProfiles("test")
 class SpringpracticeApplicationTests {
 
     @Test
