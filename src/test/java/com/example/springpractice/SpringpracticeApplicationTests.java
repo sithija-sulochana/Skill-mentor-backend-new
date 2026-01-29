9904c52a1a2198dbc454fe94b148540d7abca222
sithija_sulochana <144917814+sithija-sulochana@users.noreply.github.com>
2026-01-29 18:09:34 +0530

    REPLACE test class

--- src/test/java/com/example/springpractice/SpringpracticeApplicationTests.java
+++ src/test/java/com/example/springpractice/SpringpracticeApplicationTests.java
@@ -3,11 +3,18 @@ package com.example.springpractice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+        properties = {
+                "spring.autoconfigure.exclude=" +
+                        "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+                        "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+        }
+)
 class SpringpracticeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        // Application context loads successfully
+    }
 
 }
