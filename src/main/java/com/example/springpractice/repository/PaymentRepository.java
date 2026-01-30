17a376d7fceee6ef662c7a1c148bb48739279a59
sithija_sulochana <144917814+sithija-sulochana@users.noreply.github.com>
2026-01-30 11:51:25 +0530

    Update PaymentRepository.java

--- src/main/java/com/example/springpractice/repository/PaymentRepository.java
+++ src/main/java/com/example/springpractice/repository/PaymentRepository.java
@@ -3,5 +3,6 @@ package com.example.springpractice.repository;
 import com.example.springpractice.entites.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository 
 public interface PaymentRepository extends JpaRepository<Payment,Integer> {
 }
