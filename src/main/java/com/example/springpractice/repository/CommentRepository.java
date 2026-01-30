66676aa4a6c870edaba2fa1311156ec2e61b2b96
sithija_sulochana <144917814+sithija-sulochana@users.noreply.github.com>
2026-01-30 18:52:56 +0530

    Add @Repository annotation to repository interfaces

--- src/main/java/com/example/springpractice/repository/CommentRepository.java
+++ src/main/java/com/example/springpractice/repository/CommentRepository.java
@@ -2,8 +2,9 @@ package com.example.springpractice.repository;
 
 import com.example.springpractice.entites.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
-
+@Repository
 public interface CommentRepository extends JpaRepository<Comment,Long> {
 }
