000038ab2e86a93d00ee9336bc5ffb75c7a2b903
sithija_sulochana <144917814+sithija-sulochana@users.noreply.github.com>
2026-01-30 18:41:53 +0530

    Update MentorRepository.java

--- src/main/java/com/example/springpractice/repository/MentorRepository.java
+++ src/main/java/com/example/springpractice/repository/MentorRepository.java
@@ -3,7 +3,6 @@ package com.example.springpractice.repository;
 import com.example.springpractice.entites.Mentor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository 
 
 public interface MentorRepository extends JpaRepository<Mentor,Long> {
 }
