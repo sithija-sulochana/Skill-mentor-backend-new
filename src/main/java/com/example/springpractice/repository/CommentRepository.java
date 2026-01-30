2dbf739b70f97c8baf12642f47c4d8686f668ff1
sithija_sulochana <144917814+sithija-sulochana@users.noreply.github.com>
2026-01-30 11:51:45 +0530

    Update CommentRepository.java

--- src/main/java/com/example/springpractice/repository/CommentRepository.java
+++ src/main/java/com/example/springpractice/repository/CommentRepository.java
@@ -3,5 +3,7 @@ package com.example.springpractice.repository;
 import com.example.springpractice.entites.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository 
+
 public interface CommentRepository extends JpaRepository<Comment,Long> {
 }
