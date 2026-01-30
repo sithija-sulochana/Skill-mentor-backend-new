4c0076b9a09a72a4c12b197d644cb5ff6708eb73
sithija_sulochana <144917814+sithija-sulochana@users.noreply.github.com>
2026-01-30 11:52:06 +0530

    Update SubjectRepository.java

--- src/main/java/com/example/springpractice/repository/SubjectRepository.java
+++ src/main/java/com/example/springpractice/repository/SubjectRepository.java
@@ -3,5 +3,7 @@ package com.example.springpractice.repository;
 import com.example.springpractice.entites.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+@Repository 
 public interface SubjectRepository extends JpaRepository<Subject,Long> {
 }
